75b1ef4ad78e8bba8727fbfd9f8982d57c1d0f75
LaptevAlex <axlisradika@mail.ru>
2019-03-10 22:01:29 +0300

    Task2

---
+++ Task3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Насколько высока елочка? ");
+
+            int width = int.Parse(Console.ReadLine())+1;
+            Console.WriteLine("Раз, Два, Три - Ёлочка гори!");
+
+            for (int i = width; i > 0; i--)
+            {
+                Console.WriteLine();
+                for (int g = i; g < width; g++)
+                {
+                    Console.Write("*");
+                   
+                }
+ 
+                Console.Write(" ");
+            }
+           
+            Console.ReadKey();
+
+        }
+    }
+}
