fb5dc9dd3f55fe2b37790e975878e86c2312cad5
LaptevAlex <axlisradika@mail.ru>
2019-03-10 22:02:38 +0300

    Task5

---
+++ Task6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {int res = 0;
+            for (int counter = 0; counter <= 1000; counter++)
+            {
+                
+                int one = counter % 3;
+                int two = counter % 5;
+                
+
+                if (one == 0)
+                {
+                  
+                    res = counter + res;
+                    
+                }
+                else if(two == 0)
+                {
+                    
+                    res = counter + res;
+                    
+                }
+                
+            }
+            int otvet = res;
+
+            Console.WriteLine("Сумма всех чисел равна = "+otvet);
+            Console.ReadLine();
+            
+        }
+    }
+}
