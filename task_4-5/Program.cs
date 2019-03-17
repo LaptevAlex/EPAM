ac15753200079f97c5bb770dad5e9ae5c7985eaa
LaptevAlex <axlisradika@mail.ru>
2019-03-17 23:16:31 +0300

    task_4-5

---
+++ task_4-5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace task_4_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строку:");
+            string str = Console.ReadLine();
+            Regex pattern = new Regex("<.*?>");
+            str = pattern.Replace(str, "_");
+            Console.WriteLine("Результат :"+str);
+            Console.ReadKey();
+        }
+    }
+}
