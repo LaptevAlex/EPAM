1b1ef0f5441fc160196bb0b39cfac9a4b1f41821
LaptevAlex <axlisradika@mail.ru>
2019-03-17 23:16:58 +0300

    4-6

---
+++ task_4-6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace task_4_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            string str = Console.ReadLine();
+            Regex pattern1 = new Regex(@"^-?[0-9]+\.?[0-9]+$");
+            Regex pattern2 = new Regex(@"^-?[0-9]\.?[0-9]+e[-+]?[0-9]+$");
+            if (pattern1.IsMatch(str))
+            {
+                Console.WriteLine("Число в обычной нотации");
+            }
+            else if (pattern2.IsMatch(str))
+            {
+                Console.WriteLine("Это число в научной нотации");
+            }
+            else
+            {
+                Console.WriteLine("Это не число");
+            }
+            Console.ReadKey();
+        }
+    }
+}
