690203ce42b52606b66b7e137f50d4ff5aacdf95
LaptevAlex <axlisradika@mail.ru>
2019-03-17 23:15:56 +0300

    task_4-4

---
+++ task_4-4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace task_4_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "";
+            StringBuilder sb = new StringBuilder();
+            int N = 100;
+            Stopwatch time = new Stopwatch();
+            time.Start();
+            for (int i = 0; i < N; i++)
+            {
+                str += "*";
+            }
+            time.Stop();
+            Console.WriteLine($"string: {time.Elapsed.TotalMilliseconds}");
+            time.Restart();
+            for (int i = 0; i < N; i++)
+            {
+                sb.Append("*");
+            }
+            var s = sb.ToString();
+            time.Stop();
+            Console.WriteLine($"stringBuilder: {time.Elapsed.TotalMilliseconds}");
+            Console.ReadKey();
+        }
+    }
+}
