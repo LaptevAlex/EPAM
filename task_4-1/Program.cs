6f26c4c97f2810a220b348aa9f3e94092bb25596
LaptevAlex <axlisradika@mail.ru>
2019-03-17 23:13:25 +0300

    task_4-1

---
+++ task_4-1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_4_1
+{ 
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Введите строку:");
+                String str = Console.ReadLine();
+                Console.WriteLine("Средняя длина слова в строке "+GetLength(str)+" символов");
+                Console.ReadKey();
+            }
+
+            static public double GetLength(string str)
+            {
+                char[] arr = str.ToCharArray();
+                bool chek = false;
+                double sum = 0;
+                int numWord = 0;
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (char.IsLetter(arr[i]) && chek == false)
+                    {
+                        numWord++;
+                    }
+
+                    if (char.IsLetter(arr[i]))
+                    {
+                        chek = true;
+                        sum++;
+                    }
+
+                    else
+                    {
+                        chek = false;
+                    }
+                }
+
+                return sum / numWord;
+            }
+        }
+    }
+
